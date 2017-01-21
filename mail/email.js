5cd4ba67fdb418bd0ce4cad4a3b1f84b9c5da777
Owner-PC\hen <btnysci@yahoo.com>
2017-01-21 14:36:49 -0800

    V3222

--- mail/email.js
+++ mail/email.js
@@ -5,6 +5,27 @@ var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 // using SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 
+// var helper = require('sendgrid').mail;
+// var from_email = new helper.Email('test@example.com');
+// var to_email = new helper.Email('test@example.com');
+// var subject = 'Hello World from the SendGrid Node.js Library!';
+// var content = new helper.Content('text/plain', 'Hello, Email!');
+// var mail = new helper.Mail(from_email, subject, to_email, content);
+
+// var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+// var request = sg.emptyRequest({
+//   method: 'POST',
+//   path: '/v3/mail/send',
+//   body: mail.toJSON(),
+// });
+
+// sg.API(request, function(error, response) {
+//   console.log(response.statusCode);
+//   console.log(response.body);
+//   console.log(response.headers);
+// });
+
+
 var email = {
 
 	send: function(emSubject, email, message, callback){
@@ -25,16 +46,15 @@ var email = {
 
 			sg.API(request, function(error, response) {
 			  if(error){
-			  	throw error;
 			  	console.log(error);
 			  }   	
 			  console.log(response.statusCode)
 			  console.log(response.body)
 			  console.log(response.headers)
-			  // callback(response);
+			  callback(error);
 			});
 
-			callback();
+			// callback();
 	}
 }
 

508b1708fae16f8204af44d7af9f0feb197e2588
Owner-PC\hen <btnysci@yahoo.com>
2017-01-21 20:25:17 -0800

    Changes

--- mail/email.js
+++ mail/email.js
@@ -2,35 +2,35 @@ var helper = require('sendgrid').mail
 var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
 
-// using SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
+// // using SendGrid's v3 Node.js Library
+// // https://github.com/sendgrid/sendgrid-nodejs
 
-// var helper = require('sendgrid').mail;
-// var from_email = new helper.Email('test@example.com');
-// var to_email = new helper.Email('test@example.com');
-// var subject = 'Hello World from the SendGrid Node.js Library!';
-// var content = new helper.Content('text/plain', 'Hello, Email!');
-// var mail = new helper.Mail(from_email, subject, to_email, content);
+// // var helper = require('sendgrid').mail;
+// // var from_email = new helper.Email('test@example.com');
+// // var to_email = new helper.Email('test@example.com');
+// // var subject = 'Hello World from the SendGrid Node.js Library!';
+// // var content = new helper.Content('text/plain', 'Hello, Email!');
+// // var mail = new helper.Mail(from_email, subject, to_email, content);
 
-// var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-// var request = sg.emptyRequest({
-//   method: 'POST',
-//   path: '/v3/mail/send',
-//   body: mail.toJSON(),
-// });
+// // var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+// // var request = sg.emptyRequest({
+// //   method: 'POST',
+// //   path: '/v3/mail/send',
+// //   body: mail.toJSON(),
+// // });
 
-// sg.API(request, function(error, response) {
-//   console.log(response.statusCode);
-//   console.log(response.body);
-//   console.log(response.headers);
-// });
+// // sg.API(request, function(error, response) {
+// //   console.log(response.statusCode);
+// //   console.log(response.body);
+// //   console.log(response.headers);
+// // });
 
 
 var email = {
 
 	send: function(emSubject, senderMail, message, callback){
 			
-			var from_email = new helper.Email(email);
+			var from_email = new helper.Email(senderMail);
 			var to_email = new helper.Email('btnysci@yahoo.com');
 			var subject = emSubject;
 			var content = new helper.Content("text/plain", message);
@@ -59,22 +59,66 @@ var email = {
 }
 
 module.exports = email;
-  
-// from_email = new helper.Email("test@example.com")
-// to_email = new helper.Email("test@example.com")
-// subject = "Sending with SendGrid is Fun"
-// content = new helper.Content("text/plain", "and easy to do anywhere, even with Node.js")
-// mail = new helper.Mail(from_email, subject, to_email, content)
+
+
+
+
 
 // var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-// var request = sg.emptyRequest({
-//   method: 'POST',
-//   path: '/v3/mail/send',
-//   body: mail.toJSON()
-// });
 
-// sg.API(request, function(error, response) {
-//   console.log(response.statusCode)
-//   console.log(response.body)
-//   console.log(response.headers)
-// });
+// var email = {
+
+// 	send: function(emSubject, senderMail, message, callback){
+
+// 		var request = sg.emptyRequest({
+// 		  method: 'POST',
+// 		  path: '/v3/mail/send',
+// 		  body: {
+// 		    personalizations: [
+// 		      {
+// 		        to: [
+// 		          {
+// 		            email: 'btnysci@yahoo.com',
+// 		          },
+// 		        ],
+// 		        subject: emSubject,
+// 		      },
+// 		    ],
+// 		    from: {
+// 		      email: senderMail,
+// 		    },
+// 		    content: [
+// 		      {
+// 		        type: 'text/plain',
+// 		        value: message,
+// 		      },
+// 		    ],
+// 		  },
+// 		});
+
+// 		//With promise
+// 		sg.API(request)
+// 		  .then(response => {
+// 		    console.log(response.statusCode);
+// 		    console.log(response.body);
+// 		    console.log(response.headers);
+// 		  })
+// 		  .catch(error => {
+// 		    //error is an instance of SendGridError
+// 		    //The full response is attached to error.response
+// 		    console.log(error.response.statusCode);
+// 		  });
+
+// 		//With callback
+// 		// sg.API(request, function(error, response) {
+// 		//   if (error) {
+// 		//     console.log('Error response received');
+// 		//   }
+// 		//   console.log(response.statusCode);
+// 		//   console.log(response.body);
+// 		//   console.log(response.headers);
+// 		// });
+// 	}
+// }
+
+// module.exports = email;
